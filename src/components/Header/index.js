fac2687696912d137a720c955e3d0e1bfa536503
Diogo <di_smgomes@hotmail.com>
2023-10-07 10:41:40 -0300

    feat: Main pages default creation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { Nav } from './styled';
 
 export default function Header() {
-  const buttonClicked = useSelector(
-    (state) => state.exampleReducer.buttonClicked
-  );
   return (
     <Nav>
       <Link to="/">
         <FaHome size={24} />
       </Link>
 
-      <Link to="/login">
+      <Link to="/register">
         <FaSignInAlt size={24} />
       </Link>
 
-      <Link to="/fdf">
+      <Link to="/login">
         <FaUserAlt size={24} />
       </Link>
-      <p style={{ color: 'white' }}>{buttonClicked ? 'Open' : 'Close'}</p>
     </Nav>
   );
 }
