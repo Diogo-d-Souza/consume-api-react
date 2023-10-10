1039dad91a7c51d4f84adb7c236872e5e77d90bb
Diogo <di_smgomes@hotmail.com>
2023-10-09 21:07:43 -0300

    feat: Changing students photo

--- src/pages/Photos/index.js
+++ src/pages/Photos/index.js
@@ -1,10 +1,81 @@
-import React from 'react';
+import { get } from 'lodash';
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import Loading from '../../components/Loading';
+import axios from '../../services/axios';
+import history from '../../services/history';
+import * as actions from '../../store/modules/auth/actions';
 import { Container } from '../../styles/GlobalStyles';
+import { Form, Title } from './styled';
 
-export default function Photos() {
+export default function Photos({ match }) {
+  const dispatch = useDispatch();
+  const id = get(match, 'params.id', '');
+  const [isLoading, setIsLoading] = useState(false);
+  const [photo, setPhoto] = useState('');
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`/students/${id}`);
+        setPhoto(get(data, 'Photos[0].url', ''));
+        setIsLoading(false);
+      } catch {
+        toast.error('Failed to retrieve image');
+        setIsLoading(false);
+        history.push('/');
+      }
+    };
+    getData();
+  }, [id]);
+
+  const handleChange = async (e) => {
+    const file = e.target.files[0];
+    const photoUrl = URL.createObjectURL(file);
+
+    setPhoto(photoUrl);
+
+    const formData = new FormData();
+    formData.append('student_id', id);
+    formData.append('photo', file);
+
+    try {
+      setIsLoading(true);
+      await axios.post('/photos/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      toast.success('Photo sent successfully');
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      const { status } = get(err, 'response', '');
+      toast.error('Error while sending photo');
+
+      if (status === 401) dispatch(actions.loginFailure());
+    }
+  };
+
   return (
     <Container>
-      <h1>Photos</h1>
+      <Loading isLoading={isLoading} />
+
+      <Title>Photos</Title>
+
+      <Form>
+        <label htmlFor="photo">
+          {photo ? <img src={photo} alt="profile" /> : 'Select'}
+          <input type="file" id="photo" onChange={handleChange} />
+        </label>
+      </Form>
     </Container>
   );
 }
+
+Photos.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
